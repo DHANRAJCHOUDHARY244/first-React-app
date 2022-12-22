29eb5fd300f00adadb4bcf0e5c3b63977f55f9c2
DHANRAJCHOUDHARY244 <86722855+DHANRAJCHOUDHARY244@users.noreply.github.com>
2022-12-23 00:33:27 +0530

    modified

---
+++ src/Heading.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+function Heading() {
+  return <h1>HELLO GYS WHAT UP</h1>;
+}
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import Heading from './Heading';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const fav_palace = 'AMERICA';
+const short_name = 'USA';
+
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+const today = new Date();
+
+ReactDOM.render(
+  <div className="top">
+    <Heading />
+    <h1 contentEditable="true">CURRENT DAY IS {today.toLocaleDateString('hi-IN', options)}</h1>
+    <h3>
+      Current Time Is{' '}
+      {(today.getHours() % 12) + ':' + today.getMinutes() + ':' + today.getSeconds() + ':' + today.getMilliseconds()}
+    </h3>
+    <h4>Hii My favourite food</h4>
+    <ul>
+      <li>BANANA</li>
+      <li>KAGU KATLI</li>
+      <li>MILK SHAKE</li>
+      <li>PINE APPLE</li>
+    </ul>
+    <h5>My Favourite palace is {fav_palace}</h5>
+    <h6>Sort name is {`${fav_palace}  ${short_name}`}</h6>
+  </div>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
